7f91e30a57fc36db51ef6907bcd1b44c5cd209e0
Laird Kruger <accounts@lairdkruger.com>
2026-01-25 21:33:45 +1000

    Keymaps

--- lua/search.lua
+++ lua/search.lua
@@ -46,7 +46,8 @@ telescope.setup({
 })
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>h", builtin.help_tags, { desc = "Find Help" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>/", require("telescope.builtin").live_grep, { desc = "Find Content" })
+vim.keymap.set("n", "<leader>s", builtin.lsp_document_symbols, { desc = "Find Document Symbols" })
+vim.keymap.set("n", "<leader>S", builtin.lsp_dynamic_workspace_symbols, { desc = "Find Workspace Symbols" })
